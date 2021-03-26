ed92e5e67a0afa17d2ff60c30dd976c9d84fe1d2
nehaliacharya04 <nehaliacharya04@gmail.com>
2021-03-26 13:08:08 +0530

    removed comments

--- src/com/lld/model/Review.java
+++ src/com/lld/model/Review.java
@@ -3,25 +3,22 @@ package com.lld.model;
 public class Review {
 	private int score;
 	private Movie movie;
-	//private Person user;
 
-	public Review( Movie movie) {
+	public Review(Movie movie) {
 		this.movie = movie;
-		//this.user = user;
 	}
 
 	public void setScore(Person user, int score) {
-		if(user.getType().equals(TypeOfUsers.CRITIC)) {
+		if (user.getType().equals(TypeOfUsers.CRITIC)) {
 			this.score = score * 2;
-		}
-		else if(user.getType().equals(TypeOfUsers.VIEWER) && score >= 1 && score <= 10)
+		} else if (user.getType().equals(TypeOfUsers.VIEWER) && score >= 1 && score <= 10)
 			this.score = score;
 	}
-	
+
 	public Movie getMovie() {
 		return movie;
 	}
-	
+
 	public int getScore() {
 		return score;
 	}
